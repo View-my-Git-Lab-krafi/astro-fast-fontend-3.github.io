84240d473a4d8eaa3175cd93b9d53de39206b251
Your Name <you@example.com>
2024-07-04 22:05:00 +0100

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,35 +2,17 @@ import { defineConfig } from 'astro/config';
 
 import netlify from "@astrojs/netlify";
 
-//if you are using localhost PC
-//export default defineConfig({}); 
-
-//If you are using netlify
-//export default defineConfig({ output: "server", adapter: netlify() });
-
-
+// https://astro.build/config
+export default defineConfig({
+  output: "server",
+  adapter: netlify()
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Just To  keep some old default stuff
-
-//export default defineConfig({ output: "server", adapter: netlify() });
+/*import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-    output: "server",
-    adapter: netlify()
-  });
+  site: 'http://localhost', // Use a placeholder URL
+  // other configuration options
+});
+*/
