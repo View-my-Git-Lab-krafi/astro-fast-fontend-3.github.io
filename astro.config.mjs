4e251c53d3c48f91ca7485eb3501d2c08607cde0
kazi ar rafi <krafi.info@gmail.com>
2024-07-05 00:34:49 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import { defineConfig } from 'astro/config';
 import netlify from "@astrojs/netlify";
 
 //if you are using localhost PC
-export default defineConfig({}); 
+// export default defineConfig({}); 
 
 //If you are using netlify
 //export default defineConfig({ output: "server", adapter: netlify() });
 
 
- 
-
+export default defineConfig({ site: 'https://View-my-Git-Lab-krafi.gitlab.io',  base: '/astro-fast-fontend-3.github.io',})
 
 
 
